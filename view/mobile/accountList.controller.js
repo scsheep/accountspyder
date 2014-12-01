d2884028fb41b1f463c831b3b2892f31f0667794
scsheep <s.campbell@compriseit.co>
2014-12-01 12:22:05 +0000

    Commit of functioning list

--- view/mobile/accountList.controller.js
+++ view/mobile/accountList.controller.js
@@ -44,35 +44,13 @@ sap.ui.controller("view.mobile.accountList", {
         };
         //setup function for the model in the final app this is where we will call C4C to obtain 
         //logged in user and the accounts directly assigned to them, 
-        sap.ui.core.setModel(new sap.ui.model.json.JSONModel(myAccounts),'myAccounts');
-        sap.ui.core.setModel(new sap.ui.model.json.JSONModel(userModel),'userModel' );
+        sap.ui.getCore().setModel(new sap.ui.model.json.JSONModel(myAccounts),'myAccounts');
+        sap.ui.getCore().setModel(new sap.ui.model.json.JSONModel(userModel),'userModel' );
         
+	},
+	navigateCompany:function(){
+	
 	}
 
-/**
-* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-* (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf accountSpyder.view.mobile.accountList
-*/
-//	onBeforeRendering: function() {
-//
-//	},
-
-/**
-* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-* This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf accountSpyder.view.mobile.accountList
-*/
-//	onAfterRendering: function() {
-//
-//	},
-
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf accountSpyder.view.mobile.accountList
-*/
-//	onExit: function() {
-//
-//	}
 
 });
