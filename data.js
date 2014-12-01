38735c5e8cc03b1bc352a60c704ec22af96c9e42
scsheep <s.campbell@compriseit.co>
2014-12-01 10:43:35 +0000

    Data Tweak

--- data.js
+++ data.js
@@ -1,29 +1,40 @@
 var myAccounts = {
     accounts:[
         {
-            accountName:'Diageo',
+            accountName:"Diageo",
             pipeLineRemaining:15 ,
             totalPipeline:10 ,
-            lastWorked:'12/05/2014',
+            lastWorked:"12/05/2014"
         },
          {
-            accountName:'Mondelez',
-            pipeLineRemaining:,
-            totalPipeline:,
-            lastWorked:,
+            accountName:"Mondelez",
+            pipeLineRemaining:20,
+            totalPipeline:21,
+            lastWorked:"15/08/2014"
         },
          {
-            accountName:'Acme Accounting',
-            pipeLineRemaining:,
-            totalPipeline:,
-            lastWorked:,
+            accountName:"Acme Accounting",
+            pipeLineRemaining:1,
+            totalPipeline:7,
+            lastWorked:"20/11/2014"
         },
          {
-            accountName:'',
-            pipeLineRemaining:,
-            totalPipeline:,
-            lastWorked:,
+            accountName:"CompriseIT",
+            pipeLineRemaining:0,
+            totalPipeline:0,
+            lastWorked:"25/11/2014"
         }
         ]
-}
+};
+
+var userModel = {
+    firstName:"Sean",
+    surname:"Campbell",
+    region:"UK, Midlands",
+    cover:17,
+    currency:"GBP"
+};
+
+
+
 
