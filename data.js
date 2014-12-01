6e294dd43845749c925620ad72983556a7ce6bde
scsheep <s.campbell@compriseit.co>
2014-12-01 10:24:29 +0000

    Push to reset workspace

---
+++ data.js
@@ -0,0 +1,29 @@
+var myAccounts = {
+    accounts:[
+        {
+            accountName:'Diageo',
+            pipeLineRemaining:15 ,
+            totalPipeline:10 ,
+            lastWorked:'12/05/2014',
+        },
+         {
+            accountName:'Mondelez',
+            pipeLineRemaining:,
+            totalPipeline:,
+            lastWorked:,
+        },
+         {
+            accountName:'Acme Accounting',
+            pipeLineRemaining:,
+            totalPipeline:,
+            lastWorked:,
+        },
+         {
+            accountName:'',
+            pipeLineRemaining:,
+            totalPipeline:,
+            lastWorked:,
+        }
+        ]
+}
+
