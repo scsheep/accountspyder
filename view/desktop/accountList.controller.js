b5d2732c98ab42c8c8c210dd5516585645e7af3d
scsheep <s.campbell@compriseit.co>
2014-12-02 12:56:15 +0000

    Push of frags etc

--- view/desktop/accountList.controller.js
+++ view/desktop/accountList.controller.js
@@ -5,11 +5,28 @@ sap.ui.controller("view.desktop.accountList", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf accountSpyder.view.desktop.accountList
 */
-// 	onInit: function() {
+	onInit: function() {
+        sap.m.StandardListItem.extend("HoverList", {
+ 
+            // the control API:
+            metadata : {
+                events : {
+                    hover : {}
+                }
+            },
+            // the hover event handler:
+            onmouseover : function(evt) {   // is called when the Button is hovered - no event registration required
+                this.fireHover();
+            },
 
-// 	},
+            renderer : {}
+        });
+        
+	},
 navigateCompany:function(){
     
+},
+companyHover:function(){
+    alert("hovered");
 }
-
 });
