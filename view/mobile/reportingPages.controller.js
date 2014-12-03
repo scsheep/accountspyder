79f2eb3844b6d28a33058bb65bca7f5c0e16bcb8
Surya <s.harish@compriseit.co>
2014-12-03 10:40:43 +0000

    Adding carousel

--- view/mobile/reportingPages.controller.js
+++ view/mobile/reportingPages.controller.js
@@ -1,9 +1,9 @@
-sap.ui.controller("view.mobile.newsAggregation", {
+sap.ui.controller("view.mobile.reportingPages", {
 
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-* @memberOf accountSpyder.view.mobile.newsAggregation
+* @memberOf view.mobile.reportingPages
 */
 //	onInit: function() {
 //
@@ -12,7 +12,7 @@ sap.ui.controller("view.mobile.newsAggregation", {
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf accountSpyder.view.mobile.newsAggregation
+* @memberOf view.mobile.reportingPages
 */
 //	onBeforeRendering: function() {
 //
@@ -21,7 +21,7 @@ sap.ui.controller("view.mobile.newsAggregation", {
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf accountSpyder.view.mobile.newsAggregation
+* @memberOf view.mobile.reportingPages
 */
 //	onAfterRendering: function() {
 //
@@ -29,7 +29,7 @@ sap.ui.controller("view.mobile.newsAggregation", {
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf accountSpyder.view.mobile.newsAggregation
+* @memberOf view.mobile.reportingPages
 */
 //	onExit: function() {
 //
