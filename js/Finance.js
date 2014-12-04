8ac2593f544c3adb38d3edd1413075f62f926f98
scsheep <s.campbell@compriseit.co>
2014-12-04 16:10:51 +0000

    pushed all changes to finance

--- js/Finance.js
+++ js/Finance.js
@@ -2,23 +2,22 @@ var Finance = {
     getQuote:function(){
         var companyModel = sap.ui.getCore().getModel('companyModel').getData();
         var ticker = companyModel.ticker;
-        console.log(companyModel);
+
     //Current Data
     var yqlURL = location.protocol+"//query.yahooapis.com/v1/public/yql?q=";
     var dataFormat = "&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
     var realtimeQ = yqlURL + "select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22" + ticker + "%22)%0A%09%09&" + dataFormat;
     $(function () {
         $.getJSON(realtimeQ, function (json) {
-            companyModel.finance ={
-                dataSheet:json
-            };
-            sap.ui.getCore().setModel(new sap.ui.model.json.JSONModel(companyModel),  "companyModel");
+            sap.ui.getCore().setModel(new sap.ui.model.json.JSONModel(json.query.results.qoute),  "financeModel");
         }); //end CallBack
     });//End Anon
     },
     getHistoricData:function(startDate){
     var companyModel = sap.ui.getCore().getModel('companyModel').getData();
     var ticker = companyModel.ticker;
+
+    
     
     function zeroIndex(number){
         number = parseInt(number,10);
@@ -33,16 +32,17 @@ var Finance = {
     //Assume end date will always be today    
     var endDate = new Date();    
     endDate = ""+endDate.getFullYear()+"-"+zeroIndex(endDate.getMonth()+1)+"-"+zeroIndex(endDate.getDate());        
-        
+  
+ 
+    
     //this will be the historic data. Initial call will hit this to get first graph ???    
-    var yqlQuery = "select * from yahoo.finance.historicaldata where symbol = "+ticker+" and startDate = "+startDate+" and endDate = "+endDate;
-    var queryURL = location.protocol+"//query.yahooapis.com/v1/public/yql?q="+yqlQuery;
+    var yqlQuery = "select * from yahoo.finance.historicaldata where symbol = '"+ticker+"' and startDate = '"+startDate+"'and endDate = '"+endDate+"'";
+    var queryURL = location.protocol+"//query.yahooapis.com/v1/public/yql?q="+yqlQuery+"&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys";
     $(function(){
         $.getJSON(queryURL,function(json){
-            console.log(json)
+            sap.ui.getCore().setModel(new sap.ui.model.json.JSONModel(json.query.results.qoute),  "historicFinanceModel");
         });
     });
-    
     }
     
 };
